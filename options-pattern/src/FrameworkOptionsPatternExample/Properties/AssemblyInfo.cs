ea46246edf08e71044a43ebce102abdbc7f7e5af
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-06-12 13:47:19 +0100

    Clean up options example source folder structure

--- options-pattern/src/FrameworkOptionsPatternExample/Properties/AssemblyInfo.cs
+++ options-pattern/src/FrameworkOptionsPatternExample/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("670cf8d0-b2d9-4e12-91d9-d2f76f7cf602")]
+[assembly: Guid("57c1f843-2001-446f-840e-779d86177136")]
 
 // Version information for an assembly consists of the following four values:
 //
