8958e691ec546bb157ca1924821d9cfffea4b2ad
Ben Hall <3179852+benbhall@users.noreply.github.com>
2019-06-27 15:55:43 +0100

    Adding ASP .NET Core 2.1 guidance
    Co-authored-by: Ben Hall <3179852+benbhall@users.noreply.github.com>
    Co-authored-by: Robert David <arobert@Engineering.ukho.gov.uk>

---
+++ options-pattern/src/Core21OptionsPatternExample/Config/MySettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Core21OptionsPatternExample.Config
+{
+    public class MySettings
+    {
+        [Required]
+        public string MyString { get; set; }
+
+        [Range(1, 10, ErrorMessage = "MyNumber must be between 1 and 10")]
+        public int MyNumber { get; set; } = 19;
+
+        public Uri MyUri { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Missing config value MyBool")]
+        public bool MyBool { get; set; }
+
+        public string MyIpAddress { get; set; }
+
+        public string MyEnvString { get; set; }
+
+        public string MySecretString { get; set; }
+
+        public IEnumerable<string> Validate()
+        {
+            string pattern =
+                @"^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
+            var regex = new Regex(pattern);
+            if (string.IsNullOrEmpty(MyIpAddress) || !regex.Match(this.MyIpAddress).Success)
+                yield return $"Valid IP address not supplied for {nameof(MyIpAddress)}";
+        }
+    }
+}
---
+++ options-pattern/src/Core21OptionsPatternExample/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Core21OptionsPatternExample.Config;
+using Core21OptionsPatternExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Diagnostics;
+
+namespace Core21OptionsPatternExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptionsSnapshot<MySettings> _mySettings;
+
+        public HomeController(IOptionsSnapshot<MySettings> mySettings)
+        {
+            _mySettings = mySettings;
+        }
+
+        public IActionResult Index()
+        {
+            var configValue = _mySettings.Value;
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
